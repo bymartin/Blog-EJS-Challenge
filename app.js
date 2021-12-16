43a27990faad625d784346e3734dcdbdaa5bd087
Barry Martin <bymartin@verizon.net>
2021-12-15 18:29:41 -0600

    Add compose.ejs and add get route to app.js

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.get('/contact', (req, res) => {
     res.render('contact', {contactContent});
 });
 
+app.get('/compose', (req, res) => {
+    res.render('compose');
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
