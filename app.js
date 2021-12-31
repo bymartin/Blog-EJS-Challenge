1bf39af0b4b367ba7254e6e647fbbfc833e876a4
Barry Martin <bymartin@verizon.net>
2021-12-30 20:33:10 -0600

    Add MongoDB/Mongoose to persist data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent =
     'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
@@ -10,8 +11,6 @@ const aboutContent =
 const contactContent =
     'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
-const posts = [];
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,8 +18,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+main().catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+}
+
+// Schema
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+});
+
+// create a MODEL
+const Post = mongoose.model('Post', postSchema);
+
 app.get('/', (req, res) => {
-    res.render('home', { startingContent: homeStartingContent, posts });
+    Post.find({}, (err, posts) => {
+        res.render('home', { startingContent: homeStartingContent, posts });
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -35,27 +51,27 @@ app.get('/compose', (req, res) => {
     res.render('compose');
 });
 
-app.get('/posts/:postName', (req, res) => {
-    const requestedPostTitle = _.lowerCase(req.params.postName);
-    posts.forEach((post) => {
-        const postTitle = _.lowerCase(post.postTitle);
+app.get('/posts/:postId', (req, res) => {
+    const requestedPostId = req.params.postId;
 
-        if (postTitle === requestedPostTitle) {
-            res.render('post', {
-                postTitle: post.postTitle,
-                postContent: post.postContent,
-            });
-        } 
+    Post.findOne({ _id: requestedPostId }, (err, post) => {
+        res.render('post', {
+            title: post.title,
+            content: post.content,
+        });
     });
 });
 
 app.post('/compose', (req, res) => {
-    const post = {
-        postTitle: req.body.postTitle,
-        postContent: req.body.postContent,
-    };
-    posts.push(post);
-    res.redirect('/');
+    const post = new Post({
+        title: req.body.postTitle,
+        content: req.body.postContent,
+    });
+    post.save((err) => {
+        if (!err) {
+            res.redirect('/');
+        }
+    });
 });
 
 app.listen(3000, () => {
